0cd377b57a5c3a3e5b51cc7fbbd31af5652f094b
woozico0521 <zyy960521.com>
2018-11-01 23:03:24 +0800

    cat-eyes update

---
+++ d1031(cat-eyes)/js/movie.js
@@ -0,0 +1,114 @@
+new Vue({
+    el:'#actor',
+    data:{
+        actorList:[
+            {
+                'pic':'images/luben.webp',
+                'name':'鲁本·弗雷斯彻',
+                'job':'导演',
+            },
+            {
+                'pic':'images/tom.webp',
+                'name':'汤姆·哈迪',
+                'job':'迪·布洛克/毒液 Eddie Brock / Venom',
+            },
+            {
+                'pic':'images/mechiel.webp',
+                'name':'米歇尔·威廉姆斯',
+                'job':'安妮·韦英 Anne Weying',
+            },
+            {
+                'pic':'images/lizz.webp',
+                'name':'里兹·阿迈德',
+                'job':'德雷克博士 Dr. Carlton Drake',
+            },
+            {
+                'pic':'images/sikete.webp',
+                'name':'斯科特·黑兹',
+                'job':'Security Chief Roland Treece',
+            },
+            {
+                'pic':'images/reid.webp',
+                'name':'瑞德·斯科特',
+                'job':'Dr. Dan Lewis',
+            },
+            {
+                'pic':'images/zhenni.webp',
+                'name':'珍妮·斯蕾特',
+                'job':'Dr. Dora Skirth',
+            },
+            {
+                'pic':'images/meiluola.webp',
+                'name':'梅罗拉·沃特斯',
+                'job':'Homeless Woman Maria',
+            },
+            {
+                'pic':'images/lee.webp',
+                'name':'米歇尔·李',
+                'job':'',
+            },
+        ],
+    },
+})
+
+let side = new Vue({
+    el:'#side',
+    data:{
+        productList:[
+            {
+                'pic':'images/spiderman.jpg',
+                'name':'蜘蛛侠毒液纸牌',
+                'price':'28',
+                'rPrice':'58',
+            },
+            {
+                'pic':'images/qver.jpg',
+                'name':'Q版遮阳板纸巾盒',
+                'price':'89',
+                'rPrice':'89',
+            },
+            {
+                'pic':'images/bag.jpg',
+                'name':'卡哇伊遮阳板套',
+                'price':'39',
+                'rPrice':'39',
+            },
+            {
+                'pic':'images/3dglasses.jpg',
+                'name':'IMAX3D眼镜',
+                'price':'39',
+                'rPrice':'168',
+            },
+            {
+                'pic':'images/sticker.jpg',
+                'name':'3D眼镜贴6次装',
+                'price':'19.9',
+                'rPrice':'59',
+            },
+            {
+                'pic':'images/fangxiangp.jpg',
+                'name':'卡哇伊方向盘套',
+                'price':'99',
+                'rPrice':'99',
+            },
+            {
+                'pic':'images/three.jpg',
+                'name':'卡哇伊三件套',
+                'price':'119',
+                'rPrice':'119',
+            },
+            {
+                'pic':'images/ed3f4dfd9632f9aff4071eeae5d18b8e131139.jpg',
+                'name':'猫眼3D眼镜夹片',
+                'price':'29.9',
+                'rPrice':'69',
+            },
+            {
+                'pic':'images/49494f3816a550ea7d090ec65044426795359.jpg',
+                'name':'Q版遮阳板CD套',
+                'price':'59',
+                'rPrice':'59',
+            },
+        ],
+    },
+})
